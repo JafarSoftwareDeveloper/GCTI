127c5336f8f34e0cfff1630b54348da1beac5bc6
Pooja Pandey <Pooja Pandey@POOJA-ASUS>
2024-07-20 14:38:25 +0530

    project setup

---
+++ api/modules/Todo/Features/Create/v1/CreateTodoValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using FSH.WebApi.Todo.Persistence;
+
+namespace FSH.WebApi.Todo.Features.Create.v1;
+public class CreateTodoValidator : AbstractValidator<CreateTodoCommand>
+{
+    public CreateTodoValidator(TodoDbContext context)
+    {
+        RuleFor(p => p.Title).NotEmpty();
+        RuleFor(p => p.Note).NotEmpty();
+    }
+}
---
+++ api/modules/Todo/Features/Update/v1/UpdateTodoValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using FSH.WebApi.Todo.Persistence;
+
+namespace FSH.WebApi.Todo.Features.Update.v1;
+public class UpdateTodoValidator : AbstractValidator<UpdateTodoCommand>
+{
+    public UpdateTodoValidator(TodoDbContext context)
+    {
+        RuleFor(p => p.Title).NotEmpty();
+        RuleFor(p => p.Note).NotEmpty();
+    }
+}
